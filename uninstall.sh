c579476e0ad449843b82f44f0e3e55df0876d5ad
Erick Birbe <erickcion@gmail.com>
2012-12-25 13:55:35 -0430

    Moving funtions and variables to common.sh

--- uninstall.sh
+++ uninstall.sh
@@ -17,48 +17,9 @@
 # 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
 #
 
-SRC_DEST="rtl8192se_linux_2.6.0019.1207.2010"
-MODULE_NAME="r8192se_pci"
+source $(dirname $0)/common.sh
 
-PWD=$( pwd )/
-STATUS_SUCCESS=0
-STATUS_ERROR=1
-
-SRC_DEST="rtl8192se_linux_2.6.0019.1207.2010"
-MODULE_NAME="r8192se_pci"
-
-PWD=$( pwd )/
-STATUS_SUCCESS=0
-STATUS_ERROR=1
-
-## Functions ##
-
-# Writes a string to stderror as ERROR
-write_error()
-{
-	echo "ERROR: $*" 1>&2
-}
-
-# Writes a string to stderror as WARNING
-write_warning()
-{
-	echo "WARNING: $*" 1>&2
-}
-
-# Determines if the user is root
-is_root()
-{
-	if [[ $EUID -ne 0 ]]; then
-	   write_error "This script must be run as root"
-	   return $STATUS_ERROR
-	fi
-}
-
-# Check is root user
-if ! is_root; then
-	echo "Login as administrator user and try again."
-	exit $STATUS_ERROR
-fi
+check_is_root
 
 echo "Changing to directory \"$SRC_DEST\"..."
 cd $SRC_DEST
